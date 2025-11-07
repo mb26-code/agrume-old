dfcf07f95fa86a655eebf70d7829b479c660d761
Mehdi Bakhtar <mehdibakhtar26@gmail.com>
2025-11-07 21:44:51 +0100

    project start (configuration done)

---
+++ src/main/java/fr/umontpellier/hai925i/agrume/AgrumeApplication.java
@@ -0,0 +1,13 @@
+package fr.umontpellier.hai925i.agrume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/umontpellier/hai925i/agrume/AgrumeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.umontpellier.hai925i.agrume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
